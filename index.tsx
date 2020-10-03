70880fad3cbd7b02f6493846d7095684a6dd282f
risonakamo <hmykanae@gmail.com>
2020-10-03 00:12:08 -0400

    router deployed

--- index.tsx
+++ index.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {MemoryRouter as Router,Switch,Route,Link} from "react-router-dom";
 
 import ChartPage from "./components/chart-page/chart-page";
 
 import "./index.less";
 
+function IndexMain()
+{
+  return <Router>
+    <Switch>
+      <Route exact path="/">
+        <ChartPage/>
+      </Route>
+    </Switch>
+  </Router>;
+}
+
 function main()
 {
-  ReactDOM.render(<ChartPage/>,document.querySelector(".main"));
+  ReactDOM.render(<IndexMain/>,document.querySelector(".main"));
 }
 
 window.onload=main;
