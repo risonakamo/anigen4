011b2233d4a0e5bfff3a63b1de166b3cfcedc661
risonakamo <hmykanae@gmail.com>
2020-10-03 00:00:15 -0400

    chart page moved into component

---
+++ components/chart-page/chart-page.tsx
@@ -0,0 +1,65 @@
+import React,{useEffect,useState} from "react";
+import _ from "lodash";
+
+import ShowSection from "../show-section/show-section";
+import {queryShows} from "../../anilist-api/anilist-service";
+
+const exampleIds:number[]=[
+  112258,
+  16742,
+  109019,
+  108941,
+  21683,
+  106967,
+  104462,
+  104937
+];
+
+// these sections will come first in this order
+const showSectionOrder:string[]=[
+  "TV",
+  "TV_SHORT"
+];
+
+export default function ChartPage():JSX.Element
+{
+  // the currently loaded shows grouped by type.
+  const [shows,setShows]=useState<GroupedShowsQuery>({});
+
+  useEffect(()=>{
+    (async ()=>{
+      loadShows(exampleIds);
+    })();
+  },[]);
+
+  // load array of show ids, re-rendering show sections
+  async function loadShows(showIds:number[]):Promise<void>
+  {
+    setShows(await queryShows(showIds));
+  }
+
+  // create show sections from the current shows
+  function generateShowSections():JSX.Element[]
+  {
+    var orderedShowSections:JSX.Element[]=_.filter(_.map(showSectionOrder,(x:string)=>{
+      if (x in shows)
+      {
+        return <ShowSection title={x} shows={shows[x]} key={x}/>;
+      }
+    })) as JSX.Element[];
+
+    var remainingSectionNames:string[]=_.difference(Object.keys(shows),showSectionOrder);
+    var otherShowSections:JSX.Element[]=_.map(remainingSectionNames,(x:string)=>{
+      return <ShowSection title={x} shows={shows[x]} key={x}/>;
+    });
+
+    return [
+      ...orderedShowSections,
+      ...otherShowSections
+    ];
+  }
+
+  return <>
+    {generateShowSections()}
+  </>;
+}
--- index.tsx
+++ index.tsx
@@ -1,75 +1,13 @@
-import React,{useEffect,useState} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import _ from "lodash";
 
-import ShowSection from "./components/show-section/show-section";
-import {queryShows} from "./anilist-api/anilist-service";
+import ChartPage from "./components/chart-page/chart-page";
 
 import "./index.less";
 
-const exampleIds:number[]=[
-  112258,
-  16742,
-  109019,
-  108941,
-  21683,
-  106967,
-  104462,
-  104937
-];
-
-// these sections will come first in this order
-const showSectionOrder:string[]=[
-  "TV",
-  "TV_SHORT"
-];
-
-function IndexMain():JSX.Element
-{
-  // the currently loaded shows grouped by type.
-  const [shows,setShows]=useState<GroupedShowsQuery>({});
-
-  useEffect(()=>{
-    (async ()=>{
-      loadShows(exampleIds);
-    })();
-  },[]);
-
-  // load array of show ids, re-rendering show sections
-  async function loadShows(showIds:number[]):Promise<void>
-  {
-    setShows(await queryShows(showIds));
-  }
-
-  // create show sections from the current shows
-  function generateShowSections():JSX.Element[]
-  {
-    var orderedShowSections:JSX.Element[]=_.filter(_.map(showSectionOrder,(x:string)=>{
-      if (x in shows)
-      {
-        return <ShowSection title={x} shows={shows[x]} key={x}/>;
-      }
-    })) as JSX.Element[];
-
-    var remainingSectionNames:string[]=_.difference(Object.keys(shows),showSectionOrder);
-    var otherShowSections:JSX.Element[]=_.map(remainingSectionNames,(x:string)=>{
-      return <ShowSection title={x} shows={shows[x]} key={x}/>;
-    });
-
-    return [
-      ...orderedShowSections,
-      ...otherShowSections
-    ];
-  }
-
-  return <>
-    {generateShowSections()}
-  </>;
-}
-
 function main()
 {
-  ReactDOM.render(<IndexMain/>,document.querySelector(".main"));
+  ReactDOM.render(<ChartPage/>,document.querySelector(".main"));
 }
 
 window.onload=main;
