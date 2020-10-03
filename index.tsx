da50fd21221839caa9793d77b0ecd17fa54a8d21
risonakamo <hmykanae@gmail.com>
2020-10-02 21:32:14 -0400

    forcing certain sections to come first

--- index.tsx
+++ index.tsx
@@ -18,6 +18,12 @@ const exampleIds:number[]=[
   104937
 ];
 
+// these sections will come first in this order
+const showSectionOrder:string[]=[
+  "TV",
+  "TV_SHORT"
+];
+
 function IndexMain():JSX.Element
 {
   // the currently loaded shows grouped by type.
@@ -35,12 +41,29 @@ function IndexMain():JSX.Element
     setShows(await queryShows(showIds));
   }
 
-  const showSections=_.map(shows,(x:ShowInfo[],i:string)=>{
-    return <ShowSection title={i} shows={x} key={i}/>;
-  });
+  // create show sections from the current shows
+  function generateShowSections():JSX.Element[]
+  {
+    var orderedShowSections:JSX.Element[]=_.filter(_.map(showSectionOrder,(x:string)=>{
+      if (x in shows)
+      {
+        return <ShowSection title={x} shows={shows[x]} key={x}/>;
+      }
+    })) as JSX.Element[];
+
+    var remainingSectionNames:string[]=_.difference(Object.keys(shows),showSectionOrder);
+    var otherShowSections:JSX.Element[]=_.map(remainingSectionNames,(x:string)=>{
+      return <ShowSection title={x} shows={shows[x]} key={x}/>;
+    });
+
+    return [
+      ...orderedShowSections,
+      ...otherShowSections
+    ];
+  }
 
   return <>
-    {showSections}
+    {generateShowSections()}
   </>;
 }
 
