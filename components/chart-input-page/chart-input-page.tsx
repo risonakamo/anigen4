c59784832918c8b26d49e2e8166675de5e71aa1d
risonakamo <hmykanae@gmail.com>
2020-10-03 16:49:24 -0400

    split chart input box into component

--- components/chart-input-page/chart-input-page.tsx
+++ components/chart-input-page/chart-input-page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+import ChartInputBox from "./components/chart-input-box/chart-input-box";
+
 import "./chart-input-page.less";
 
 export default function ChartInputPage()
 {
   return <div className="chart-input-zone">
-    <textarea className="chart-input"></textarea>
+    <ChartInputBox/>
     <div className="buttons-zone">
       <Link to="/chart" className="generate-button">Generate Chart</Link>
     </div>
---
+++ components/chart-input-page/components/chart-input-box/chart-input-box.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./chart-input-box.less";
+
+export default function ChartInputBox()
+{
+  return <textarea className="chart-input"></textarea>;
+}
