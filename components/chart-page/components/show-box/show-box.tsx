16dcc70b5b7ca832aeebefdbf00eb5150b5ec556
risonakamo <hmykanae@gmail.com>
2020-10-05 22:14:41 -0400

    generating start date text add luxon

--- components/chart-page/components/show-box/show-box.tsx
+++ components/chart-page/components/show-box/show-box.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import _ from "lodash";
+import {DateTime} from "luxon";
 
 import "./show-box.less";
 
@@ -10,15 +11,17 @@ interface ShowBoxProps
 
 export default function ShowBox(props:ShowBoxProps):JSX.Element
 {
-  const genretags=_.map(props.show.genres,(x:string,i:number)=>{
+  var genretags:JSX.Element[]=_.map(props.show.genres,(x:string,i:number)=>{
     return <span key={i}>{x}</span>
   });
+
+  var startDate:string=createDateText(props.show.startDate.month,props.show.startDate.day);
 
   return <div className="show-box">
     <div className="top-contain">
       <span className={`season ${props.show.season}`}>{props.show.season}</span>
       <span>{props.show.seasonYear}</span>
-      <span className="date-field">Aug 20</span>
+      <span className="date-field">{startDate}</span>
     </div>
 
     <img src={props.show.coverImage.large}/>
@@ -30,4 +33,10 @@ export default function ShowBox(props:ShowBoxProps):JSX.Element
       </div>
     </div>
   </div>;
+}
+
+// format show start date
+function createDateText(month:number,day:number):string
+{
+  return DateTime.fromObject({month,day}).toFormat("MMM dd");
 }
